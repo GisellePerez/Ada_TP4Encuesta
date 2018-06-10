dea29a118ec5c239c7663ed484136555c88e62a8
GisellePerez <giselleperez91@gmail.com>
2018-06-10 16:49:01 -0300

    tomar valores de as preguntas

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -16,3 +16,13 @@ $.ajax({
         $("#paises").append(option);
     })
 })
+
+$('#enviar').on('click', function(){
+	let pais = $('#paises option:selected').val();
+	let lenguaje = ($('input:radio[name=lenguaje]:checked').val());
+	let nivel = ($('input:radio[name=nivel]:checked').val());
+	let sistema = ($('input:radio[name=sistema]:checked').val());
+	let editor = ($('input:radio[name=editor]:checked').val());
+	console.log(pais+lenguaje+nivel+sistema+editor);
+})
+
