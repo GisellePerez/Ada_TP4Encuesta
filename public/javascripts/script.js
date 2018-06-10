89b9f7d2b8f72aa6082f53a4c651afacfaa8aad4
GisellePerez <giselleperez91@gmail.com>
2018-06-10 17:10:44 -0300

    append

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,6 +6,8 @@
 //     }) 
 // });
 
+
+
 $.ajax({
 	url: "/paises",
 	dataType: "json",
@@ -18,11 +20,21 @@ $.ajax({
 })
 
 $('#enviar').on('click', function(){
+	//var respuesta = [];
+
 	let pais = $('#paises option:selected').val();
 	let lenguaje = ($('input:radio[name=lenguaje]:checked').val());
 	let nivel = ($('input:radio[name=nivel]:checked').val());
 	let sistema = ($('input:radio[name=sistema]:checked').val());
 	let editor = ($('input:radio[name=editor]:checked').val());
 	console.log(pais+lenguaje+nivel+sistema+editor);
+
+	//respuesta.push(pais,lenguaje, nivel, sistema, editor);
+	//console.log(respuesta);
+
+	let fila = `<tr> <input type="checkbox"> <td>${pais}</td> <td>${lenguaje}</td>
+				<td>${nivel}</td> <td>${sistema}</td> <td>${editor}</td> </tr>`;
+
+	$('#table').append(fila);
 })
 
