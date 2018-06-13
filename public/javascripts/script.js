4c608f6d601b706fe0547bf2638cb99069d08d08
GisellePerez <giselleperez91@gmail.com>
2018-06-13 12:59:41 -0300

    algunos estilos

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -6,8 +6,6 @@
 //     }) 
 // });
 
-
-
 $.ajax({
 	url: "/paises",
 	dataType: "json",
@@ -26,14 +24,10 @@ $.ajax({
 	console.log(data.preguntas)
 	data.preguntas.forEach(function(element) {
 		let pregunta = $(`<div id="${element.name}"><h3>${element.pregunta}</h3></div>`);
-		//console.log(element.pregunta+' '+element.opciones);
 		element.opciones.forEach(function(elem) {
-			var respuestas = $(`<label for="${elem}"><input type='radio' name='${element.name}' value='${elem}' id='${elem}'>${elem}</label>`);
-		
+			var respuestas = $(`<label for="${elem}"><input type='radio' class="radioBtn" name='${element.name}' value='${elem}' id='${elem}'>${elem}</label>`);
 			pregunta.append(respuestas);
-
-		})
-		
+		})		
 		$('form').append(pregunta);
 	})
 	$('form').append($(`<div class="btn"><input id="enviar" type='button' value='Enviar'></div>`));
@@ -41,20 +35,15 @@ $.ajax({
 })
 
 	$(document).on('click', "#enviar" ,function(){
-		//var respuesta = [];
-	
 		let pais = $('#paises option:selected').val();
 		let lenguajes = ($('input:radio[name=lenguajes]:checked').val());
 		let nivel = ($('input:radio[name=nivel]:checked').val());
 		let sistema = ($('input:radio[name=sistema]:checked').val());
 		let editor = ($('input:radio[name=editor]:checked').val());
-		// console.log(pais+lenguaje+nivel+sistema+editor);
-	
-		//respuesta.push(pais,lenguaje, nivel, sistema, editor);
-		//console.log(respuesta);
+
 		if (pais && lenguajes && nivel && sistema && editor) {
 			let fila = `<tr><td><input type="checkbox"></td> <td>${pais}</td> <td>${lenguajes}</td>
-						<td>${nivel}</td> <td>${sistema}</td> <td>${editor}</td> </tr>`;
+						<td>${nivel}</td> <td>${sistema}</td> <td>${editor}</td><td><i class="fas fa-trash"></i></td> </tr>`;
 	
 			$('#table').append(fila);
 			$("form")[0].reset();
@@ -64,5 +53,10 @@ $.ajax({
 		else {
 			$(".error").text("Faltan datos").css("display", "block")
 		}
+
+		$('.fas').on('click', function(){
+			console.log($(this).parent); //NO :( 
+			//this.parents.remove();
+		});
 	})
 
