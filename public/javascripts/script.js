d9bc2c63ba200eafe316f5340187fe9b2d1406ec
Mercedes <mechamena@gmail.com>
2018-06-14 17:19:55 -0300

    localStorage para respuestas

--- public/javascripts/script.js
+++ public/javascripts/script.js
@@ -5,7 +5,6 @@
 //         $("#paises").append(option);
 //     }) 
 // });
-
 $.ajax({
 	url: "/paises",
 	dataType: "json",
@@ -34,29 +33,59 @@ $.ajax({
 	$('form').append($("<div class='error' style='display:none'></div>"));
 })
 
-	$(document).on('click', "#enviar" ,function(){
-		let pais = $('#paises option:selected').val();
-		let lenguajes = ($('input:radio[name=lenguajes]:checked').val());
-		let nivel = ($('input:radio[name=nivel]:checked').val());
-		let sistema = ($('input:radio[name=sistema]:checked').val());
-		let editor = ($('input:radio[name=editor]:checked').val());
+function mostrarRespuestas(respuesta) {
+		let fila = `<tr data-id="${respuesta.id}"><td><input type="checkbox"></td> <td>${respuesta.pais}</td> <td>${respuesta.lenguajes}</td><td>${respuesta.nivel}</td> <td>${respuesta.sistema}</td> <td>${respuesta.editor}</td><td><i class="fas fa-trash"></i></td> </tr>`;
+		$('#table').append(fila);
+}
 
-		if (pais && lenguajes && nivel && sistema && editor) {
-			let fila = `<tr><td><input type="checkbox"></td> <td>${pais}</td> <td>${lenguajes}</td>
-						<td>${nivel}</td> <td>${sistema}</td> <td>${editor}</td><td><i class="fas fa-trash"></i></td> </tr>`;
-	
-			$('#table').append(fila);
-			$("form")[0].reset();
-			$(".error").css("display", "none");
-		}
-	
-		else {
-			$(".error").text("Faltan datos").css("display", "block")
-		}
+//trae las respuestas de localStorage o crea el array para las futuras respuestas
+let respuestas = (JSON.parse(localStorage.getItem("respuestas")) || []);
+//muestra las respuestas en la tabla
+if (respuestas.length > 0) {
+	respuestas.forEach(mostrarRespuestas)
+}
 
-		$('.fas').on('click', function(){
-			// console.log($(this).parent); //NO :( 
-			$(this).closest("tr").remove();
-		});
-	})
+$(document).on('click', "#enviar" ,function(){
+	let pais = $('#paises option:selected').val();
+	let lenguajes = ($('input:radio[name=lenguajes]:checked').val());
+	let nivel = ($('input:radio[name=nivel]:checked').val());
+	let sistema = ($('input:radio[name=sistema]:checked').val());
+	let editor = ($('input:radio[name=editor]:checked').val());
+
+	if (pais && lenguajes && nivel && sistema && editor) {
+		//guardo la respuesta en localStorage
+		let	respuesta = {
+			pais: pais,
+			lenguajes: lenguajes,
+			nivel: nivel,
+			sistema: sistema,
+			editor: editor
+		}
+		respuestas.push(respuesta);
+		let id = respuestas.indexOf(respuesta);
+		//cuando ya la mandé al array de respuestas, creo un id con el índice
+		respuesta.id = id;
+		console.log(respuesta, id)
+
+		localStorage.setItem("respuestas", JSON.stringify(respuestas));
+		//muestro la respuesta
+		mostrarRespuestas(respuesta);
+		$("form")[0].reset();
+		$(".error").css("display", "none");
+	}
+
+	else {
+		$(".error").text("Faltan datos").css("display", "block")
+	}
+})
+
+$(document).on('click', ".fas", function(){
+	// console.log($(this).parent); //NO :(
+	//saca la fila de la tabla
+	$(this).closest("tr").remove();
+	//saca la respuesta de localStorage y lo actualiza
+	let id = $(this).closest("tr").data("id");
+	respuestas.splice(id, 1);
+	localStorage.setItem("respuestas", JSON.stringify(respuestas));
+});
 
